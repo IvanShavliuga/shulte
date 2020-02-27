718b9bb49703801e857ab75e34cb4594022ff942
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-02-27 12:11:46 +0300

    Added timer

--- src/Gameboard.vue
+++ src/Gameboard.vue
@@ -1,4 +1,5 @@
 <template>
+<section>
 <div class="game">
 <table>
  <tr v-for="(r,k1) in board" :key="k1">
@@ -11,12 +12,75 @@
   </tr>    
 
 </table>
-{{sc}}
+
 </div>
+<div class="scorestable">
+<p class="status">{{status}} <span class="time">{{time}} s</span></p>
+  <p class="scores">Очки: {{scores}}</p>
+  <p>Таблица Шульте. Генерируется случайным образом поле (5 на 5). Собирайте числовую последовательность от 1 до 25 строго по порядку. </p>
+  <button @click="start">Start</button><br>
+  <p>{{CurrentTime.hour+":"+CurrentTime.minute+":"+CurrentTime.second}}</p> 
+</div>
+</section>
 </template>
+<style lang="scss">
+ body{
+  background-color:grey;
+  font-family: "Georgia";
+    table{
+        background-color:#fbc;
+        border:4px outset #fbc;
+        border-collapse:collapse;
+      td{
+        border:1px solid #c99;
+        width:40px;
+        height:40px;
+        color:#00f;
+        text-align:center;
+        font-size:1.5em;
+      }
+      td:hover{
+        background-color:#d0d;
+      }
+      .select {
+        color: white;
+        background-color:#d00;
+      } 
+      .success {
+        color: white;
+        background-color:#0d0;
+      } 
+      }
+  .game{
+    display:flex;
+    justify-content:center;
+    margin-bottom: 40px;
+    margin-top: 40px;
+  }
+  .scorestable {
+    p{
+      margin:0 auto;
+      color:white;
+      font-weight:500;
+      letter-spacing:1px;
+      text-align:center;
+      width:400px;
+    
+    }
+    .status{
+      color:#0f0;
+    }
+    .scores{
+      color:#ff0;
+    }
+  }  
+}
+
+</style>
 <script>
+import {current} from './timer';
 export default {
-   props:{
+   /*props:{
        status: {
            type:String,
            required:true 
@@ -26,7 +90,7 @@ export default {
            required: true
        }
          
-   },//props
+   },*///props
    data() {
        return {
            board: [
@@ -37,7 +101,10 @@ export default {
               [{val:1,click:false,yes:false},{val:1,click:false,yes:false},{val:1,click:false,yes:false},{val:1,click:false,yes:false},{val:1,click:false,yes:false}]     
            ],
            count:0,
-           sc:0  
+           scores:0,
+           time:59,
+           CurrentTime: current,
+           status:"You game ..."  
        } //return
    }, //data
    methods: {
@@ -47,16 +114,30 @@ export default {
            if(this.count<25)
               val = this.board[k1][k2].val;
            if(val-this.count!=1)
-              this.sc-=5;
+              this.scores-=5;
            else{
               this.board[k1][k2].yes=true; 
               this.count++;
-              this.sc+=5;
+              this.scores+=5;
            }//else
-       }//select
+       },//select
+       timer() {
+           this.time--;
+           this.status = "You game ...";
+           if(this.time===0){  
+               this.status= "Game over";    
+               this.count = 25;
+           }
+           if(this.count!=25) 
+               setTimeout(this.timer,1000)
+       },//timer
+       start() {
+           setInterval(()=>{this.timer();}, 1000);
+       }   
    },//methods
    created() {
-       
+       current.now();
+       setInterval(function() {current.now();}, 1000);
        let array=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
        for(let i=0; i<25; i++){
           let b=array[i];
@@ -78,7 +159,7 @@ export default {
           }
        } 
        this.count=0;
-       this.sc=0;
+       this.scores=0;
    }//created
 }    
 </script>
---
+++ src/timer.js
@@ -0,0 +1,27 @@
+export let current = {
+    hour:0,
+    minute:0,
+    second:0, 
+    now(){
+        let d=new Date();
+       
+        this.hour=d.getHours();
+        this.minute=d.getMinutes();
+        this.second=d.getSeconds();
+        /*$("#timer").text(this.hour+":" +this.minute+":"+this.second);
+      */
+     } 
+ } ;
+window.onload = function() {
+  /* setInterval(()=>{
+ let dt=new Date();
+    
+current.hour=dt.getHours();
+current.minute=dt.getMinutes();
+current.second=dt.getSeconds(); 
+$("#timer").text(current.hour+":"+current.minute+":"+current.second); 
+   }, 1000);*/
+  current.now();
+  setInterval(function() {current.now();}, 1000);
+  
+ };
